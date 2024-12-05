2bbe0a2542a2186813facf8919c6a79817550b12
Sandrian Syafri <id.sandriansyafri@gmail.com>
2024-12-06 00:39:03 +0700

    feat(product): add create product form

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,12 @@
 import LoadingTabelContent from "@/components/loading-tabel-content";
 import { useGetProduct } from "@/features/product/useProduct";
-import { Container, Heading, Table } from "@chakra-ui/react"
+import { Box, Container, Flex, Heading, NativeSelectField, NativeSelectRoot, Table, Textarea } from "@chakra-ui/react"
+import { Button, Fieldset, Input, Stack } from "@chakra-ui/react"
+import { Field } from "@/components/ui/field"
+import { Card } from "@chakra-ui/react"
+
+
+
 
 
 export default function Home() {
@@ -20,7 +26,7 @@ export default function Home() {
 
   return (
     <>
-      <Container maxW="container.lg" p="4">
+      <Container key={'container-product'} maxW="5xl" p="6">
         <Heading>Products</Heading>
         <Table.Root size="sm" interactive>
           <Table.Header>
@@ -37,6 +43,41 @@ export default function Home() {
           </Table.Body>
         </Table.Root>
       </Container>
+      <Container key={'container-form'} maxW="5xl" p="6">
+        <Card.Root>
+          <Card.Body>
+            <Heading mb={4}>Create Product</Heading>
+            <form action="">
+              <Fieldset.Root>
+
+                <Fieldset.Content>
+                  <Field label="Product">
+                    <Input name="product" type="text" />
+                  </Field>
+
+                  <Field label="Price">
+                    <Input name="price" type="number" />
+                  </Field>
+
+                  <Field label="Description">
+                    <Textarea name="description" type="text" rows={4} />
+                  </Field>
+
+                  <Field label="Image">
+                    <Input name="image" type="text" />
+                  </Field>
+
+                  <Button type="submit" alignSelf="flex-start">
+                    Submit
+                  </Button>
+                </Fieldset.Content>
+                
+              </Fieldset.Root>
+            </form>
+          </Card.Body>
+        </Card.Root>
+      </Container>
+
     </>
   )
 
